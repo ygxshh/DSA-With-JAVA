4cde0e2a8d1dbebda5d5acd49263256173b21fc3
Yogesh Mishra <iamyogesh121@gmail.com>
2024-11-18 01:23:08 +0530

    DSA

---
+++ src/OOPS/Encapsulation.java
@@ -0,0 +1,37 @@
+package OOPS;
+
+class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age must be positive!");
+        }
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Yogesh");
+        person.setAge(21);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        person.setAge(-5);
+    }
+}
